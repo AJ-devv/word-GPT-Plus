138284f11f589842fbf23aca8e6fd7890c116550
Ajay <anandshah207@gmail.com>
2025-06-09 01:34:25 -0700

    fix(vite): add base path to support iframe and Netlify rendering

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './', // ✅ This is CRITICAL so Netlify uses relative paths
   server: {
     port: 3000,
     https:
